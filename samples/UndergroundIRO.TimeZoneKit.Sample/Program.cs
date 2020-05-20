2395992ff034d12a97e25af80af73dcfa9d7c270
ExtremeDotneting <yuram1inbox@gmail.com>
2020-05-20 16:37:04 +0300

    Can get timezone by city in english.

---
+++ samples/UndergroundIRO.TimeZoneKit.Sample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using NodaTime;
+
+namespace UndergroundIRO.TimeZoneKit.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var searchStr = "Kiev";
+            var tz = DateTimeZoneSearch.Inst.GetTimeZone(searchStr);
+            var offsetMilliseconds = tz.GetUtcOffset(Instant.FromDateTimeUtc(DateTime.UtcNow)).Milliseconds;
+            var offsetHours = offsetMilliseconds / 1000 / 60 / 60;
+            Console.WriteLine($"Search string: {searchStr}");
+            Console.WriteLine($"Id: {tz.Id}");
+            Console.WriteLine($"Offset: {offsetHours} hours\n");
+
+            Console.WriteLine("\n\n");
+            var ids = File.ReadAllLines("timezone_cities/original.txt");
+            var strToWrite = "";
+            foreach (var id in ids)
+            {
+                var str = id;
+                var index = str.IndexOf("/");
+                if (index > 0)
+                {
+                    str = str.Substring(index + 1);
+                }
+                index = str.IndexOf("/");
+                if (index > 0)
+                {
+                    str = str.Substring(index + 1);
+                }
+
+                str = str.Replace("_", " ");
+                strToWrite += str+ "\n" ;
+            }
+
+            File.WriteAllText("timezone_cities/english.txt", strToWrite);
+
+            //var ids = DateTimeZoneProviders.Tzdb.Ids;
+            //var tz = GetTimeZone(location);
+            //var s = tz.GetUtcOffset(Instant.FromDateTimeUtc(DateTime.UtcNow)).Seconds;
+            //var hours = s / 60 / 60;
+            //await SendTextMessageAsync(hours.ToString());
+            //await SendTextMessageAsync(JsonConvert.SerializeObject(tz));
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/UndergroundIRO.TimeZoneKit/DateTimeZoneSearch.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using IRO.EmbeddedResources;
+using NodaTime;
+
+namespace UndergroundIRO.TimeZoneKit
+{
+    public class DateTimeZoneSearch
+    {
+        public static DateTimeZoneSearch Inst { get; } = new DateTimeZoneSearch();
+
+        readonly string _translatesPath;
+
+        readonly IList<string> _original;
+
+        readonly IDictionary<string, IDictionary<string, int>> _translateByFile = new ConcurrentDictionary<string, IDictionary<string, int>>();
+
+        readonly IList<IDictionary<string, int>> _translates = new List<IDictionary<string, int>>();
+
+        DateTimeZoneSearch()
+        {
+            _translatesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "timezone_cities");
+            var assembly = Assembly.GetExecutingAssembly();
+            assembly.ExtractEmbeddedResourcesDirectory("UndergroundIRO.TimeZoneKit.CityTranslates", _translatesPath);
+
+            Directory.GetFiles(_translatesPath, "*.txt");
+
+        }
+
+        bool TryGetTimeZoneFromTranslate(IDictionary<string, int> translateDict, string location, out DateTimeZone tz)
+        {
+            if (translateDict.TryGetValue(location, out var index))
+            {
+                var fullTzId=_original[index];
+                tz = DateTimeZoneProviders.Tzdb.GetZoneOrNull(fullTzId);
+                return true;
+            }
+            else
+            {
+                tz = null;
+                return false;
+            }
+        }
+
+        public DateTimeZone GetTimeZone(string location)
+        {
+            var prefixes = new[]
+            {
+                "Europe",
+                "Asia",
+                "Africa",
+                "America",
+                "America/Argentina",
+                "America/Indiana",
+                "America/North_Dakota",
+                "America/Kentucky",
+                "Antarctica",
+                "Arctic",
+                "Atlantic",
+                "Australia",
+                "Brazil",
+                "Canada",
+                "Chile",
+                "Indian",
+                "Mexico",
+                "Pacific",
+                "US"
+            };
+
+            if (TryGetTimeZoneWithPrefix(location, out var tz))
+            {
+                return tz;
+            }
+
+            foreach (var prefix in prefixes)
+            {
+                if (TryGetTimeZoneWithPrefix($"{prefix}/{location}", out var tzLoc))
+                {
+                    return tzLoc;
+                }
+            }
+            throw new Exception($"Can't find timezone for '{location}'");
+        }
+
+        static bool TryGetTimeZoneWithPrefix(string fullLocation, out DateTimeZone tz)
+        {
+            tz = DateTimeZoneProviders.Tzdb.GetZoneOrNull(fullLocation);
+            return tz != null;
+        }
+    }
+}
